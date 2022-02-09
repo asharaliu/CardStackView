972cfe6cd30b72a3e63ad057a5feb0ee41d9f0cd
asharalivu <asharali.u@byjus.com>
2022-02-09 13:21:20 +0530

    Initial commit

---
+++ sample/src/main/java/com/yuyakaido/android/cardstackview/sample/CardStackAdapter.kt
@@ -0,0 +1,38 @@
+package com.yuyakaido.android.cardstackview.sample
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+
+class CardStackAdapter(
+        private var htmlList: List<String> = emptyList()
+) : RecyclerView.Adapter<CardStackAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return ViewHolder(inflater.inflate(R.layout.item_spot, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val spot = htmlList[position]
+        val mimeType = "text/html"
+        val encoding = "UTF-8"
+
+        holder.vwContainer.loadDataWithBaseURL("", spot, mimeType, encoding, "")
+        holder.itemView.setOnClickListener { v ->
+            Toast.makeText(v.context, "$position Clicked", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return htmlList.size
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val vwContainer :WebView = view.findViewById(R.id.wvContainer)
+    }
+
+}
